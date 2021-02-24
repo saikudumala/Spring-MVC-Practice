2d4f61a28478401e244151669042f037d6a9477c
saiyaswanth.kudumala@gmail.com <github.open123>
2021-02-23 19:15:06 -0500

    Boot application for using Query Parameters

---
+++ Spring-WebMVC-Form-3/src/main/java/com/yaswanth/SpringWebMvcForm3Application.java
@@ -0,0 +1,13 @@
+package com.yaswanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcForm3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcForm3Application.class, args);
+	}
+
+}
---
+++ Spring-WebMVC-Form-3/src/main/java/com/yaswanth/controller/CustomerController.java
@@ -0,0 +1,44 @@
+package com.yaswanth.controller;
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+
+//Practicing query and path parameters
+public class CustomerController {
+	@GetMapping("/customerDetails")
+	public String customerDetails(@RequestParam Integer customerId,Model model) {
+		if(customerId==101) {
+			model.addAttribute("msg","Sai Yaswanth");
+		}
+		else if(customerId==102) {
+			model.addAttribute("msg", "Hari Charan");
+		}
+		else if(customerId==103) {
+			model.addAttribute("msg","Chandra Sekhar");
+		}
+		return "index";
+	}
+	@GetMapping("/AllcustomerDetails")
+	public String allCustomerDetails(@RequestParam Integer [] id,Model model) {
+		model.addAttribute("msg",Arrays.toString(id));
+		return "index";
+	}
+	@GetMapping("/areastats")
+	public String getCustomersCount(@RequestParam("area") String area,@RequestParam("age") Integer age, Model model) {
+		String response = null;
+		if(area.equals("Hyderabad")&& age>=30) {
+			response="we have 110 customers in Hyderabad";
+		}
+		else if(area.equals("Bangalore")&& age>=25) {
+			response="we have 150 customers in Hyderabad";
+		}
+		model.addAttribute("msg",response);
+		return "index";
+	}
+	}
+	
+	
